1ee5ef4c62a81253db0aba733699b0679182a1b6
YH-Choi-001 <84622891+YH-Choi-001@users.noreply.github.com>
2026-05-16 13:10:55 +0100

    refactor(bt): create facade class

--- frontend/src/lib/orientation/orientation.ts
+++ frontend/src/lib/orientation/orientation.ts
@@ -1,28 +1,29 @@
 /**
  * Request permission to listen to device orientation and attach an event handler.
- * 
+ *
  * @param listener The event handler to attach to the device orientation event.
  */
 
-const ORIENTATION_EVENT = "deviceorientation";
+const ORIENTATION_EVENT = 'deviceorientation'
 
 function attachEventHandler(listener: (e: DeviceOrientationEvent) => void): void {
-    window.addEventListener(ORIENTATION_EVENT, listener);
+  window.addEventListener(ORIENTATION_EVENT, listener)
 }
 
 function requestListenToOrientation(listener: (e: DeviceOrientationEvent) => void): void {
-    if ('requestPermission' in DeviceOrientationEvent && typeof DeviceOrientationEvent.requestPermission === "function") {
-        // The API requires permission — request it
-        Promise.all([
-            DeviceOrientationEvent.requestPermission(),
-        ]).then(([orientationPermission]) => {
-            if (orientationPermission === "granted") {
-                attachEventHandler(listener);
-            }
-        });
-    } else {
-        attachEventHandler(listener);
-    }
+  if (
+    'requestPermission' in DeviceOrientationEvent &&
+    typeof DeviceOrientationEvent.requestPermission === 'function'
+  ) {
+    // The API requires permission — request it
+    Promise.all([DeviceOrientationEvent.requestPermission()]).then(([orientationPermission]) => {
+      if (orientationPermission === 'granted') {
+        attachEventHandler(listener)
+      }
+    })
+  } else {
+    attachEventHandler(listener)
+  }
 }
 
-export { requestListenToOrientation };
+export { requestListenToOrientation }
