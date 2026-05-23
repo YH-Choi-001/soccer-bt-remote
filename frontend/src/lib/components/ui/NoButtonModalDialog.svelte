2d920a8db8f2f687eb186f738d7b27b62d54eca7
YH-Choi-001 <84622891+YH-Choi-001@users.noreply.github.com>
2026-05-23 03:11:54 +0100

    fix(dialog): fix dialog show/hide logic

--- frontend/src/lib/components/ui/NoButtonModalDialog.svelte
+++ frontend/src/lib/components/ui/NoButtonModalDialog.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
   import type { Snippet } from 'svelte'
+  import type { Attachment } from 'svelte/attachments'
+  import { on } from 'svelte/events'
 
   let {
     isOpen = $bindable(false),
@@ -9,30 +11,27 @@
     children: Snippet
   } = $props()
 
-  let dialog = $state<HTMLDialogElement | undefined>(undefined)
-
-  const onCancel = (e: Event) => {
-    // We consume the cancel event to prevent the dialog from closing immediately.
-    // Instead, we set `isOpen` to false to play the transition.
-    e.preventDefault()
-    isOpen = false
-  }
-
-  $effect(() => {
+  const onToggled: Attachment<HTMLDialogElement> = (dialog: HTMLDialogElement) => {
     if (isOpen) {
-      dialog?.showModal()
-      dialog?.addEventListener('cancel', onCancel)
+      dialog.showModal()
+      on(dialog, 'cancel', () => {
+        isOpen = false
+      })
+    } else {
+      dialog.close()
     }
-    // No need to close the dialog here,
-    // because the dialog will be unbinded when `isOpen` is set to false.
-  })
+  }
 </script>
 
-<dialog bind:this={dialog} class="modal">
+<dialog {@attach onToggled} class="modal">
   <div class="modal-box">
     {@render children()}
   </div>
   <form method="dialog" class="modal-backdrop">
-    <button>close</button>
+    <button
+      onclick={() => {
+        isOpen = false
+      }}>close</button
+    >
   </form>
 </dialog>
