d24a163a48cea06e360e450eefc708c48bd6c4e8
YH-Choi-001 <84622891+YH-Choi-001@users.noreply.github.com>
2026-05-30 22:08:29 +0100

    feat(routing): add routing to debug pages

---
+++ frontend/src/routes/debug/+page.svelte
@@ -0,0 +1,23 @@
+<!--
+Debug page.
+-->
+
+<script lang="ts">
+  import OptionsPage from '$lib/components/ui/OptionsPage.svelte'
+  import { resolve } from '$app/paths'
+
+  let options = [
+    {
+      title: 'Bluetooth Terminal',
+      subtitle: 'Communicate with robot via bluetooth terminal.',
+      link: resolve('/debug/bluetooth'),
+    },
+    {
+      title: 'Gyroscope Test',
+      subtitle: 'Check gyroscope data in real-time.',
+      link: resolve('/debug/gyro'),
+    },
+  ]
+</script>
+
+<OptionsPage {options} />
