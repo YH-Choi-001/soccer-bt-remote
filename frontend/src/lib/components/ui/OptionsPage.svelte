1c87bc2b3f4b439126e0c13f473153859df053b5
YH-Choi-001 <84622891+YH-Choi-001@users.noreply.github.com>
2026-05-30 21:54:13 +0100

    feat(routing): add pages for routing

---
+++ frontend/src/lib/components/ui/OptionsPage.svelte
@@ -0,0 +1,46 @@
+<!--
+Home page.
+-->
+
+<script lang="ts">
+  import Card from '$lib/components/ui/Card.svelte'
+  import FooterWrappedPage from '$lib/components/ui/FooterWrappedPage.svelte'
+  import type { Snippet } from 'svelte'
+  import type { ResolvedPathname } from '$app/types'
+
+  let {
+    options,
+  }: {
+    options: Array<{
+      title: string
+      subtitle: string
+      link: ResolvedPathname
+      image?: Snippet
+    }>
+  } = $props()
+</script>
+
+{#snippet card(
+  title: string,
+  subtitle: string,
+  link: ResolvedPathname,
+  image: Snippet | undefined = undefined
+)}
+  <Card {title} {subtitle} {image}>
+    {#snippet action()}
+      <a href={link}>Start Using</a>
+    {/snippet}
+  </Card>
+{/snippet}
+
+<FooterWrappedPage>
+  <div class="p-4">
+    <h1 class="mt-4 mb-6 text-center text-2xl">Welcome to Soccer Bluetooth Remote Controller</h1>
+
+    <div class="flex flex-col items-center justify-evenly gap-4 sm:flex-row">
+      {#each options as option (option.title + option.subtitle + option.link)}
+        {@render card(option.title, option.subtitle, option.link, option.image)}
+      {/each}
+    </div>
+  </div>
+</FooterWrappedPage>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -3,35 +3,21 @@ Home page.
 -->
 
 <script lang="ts">
-  import { requestListenToOrientation } from '$lib/orientation/orientation'
+  import OptionsPage from '$lib/components/ui/OptionsPage.svelte'
+  import { resolve } from '$app/paths'
 
-  let alpha = $state(0)
-  let beta = $state(0)
-  let gamma = $state(0)
-  let timeStamp = $state(0)
-
-  function handleOrientation(e: DeviceOrientationEvent) {
-    if (e.alpha != null) alpha = e.alpha
-    if (e.beta != null) beta = e.beta
-    if (e.gamma != null) gamma = e.gamma
-    if (e.timeStamp != null) timeStamp = e.timeStamp
-    console.log(e.alpha, e.beta, e.gamma, e.timeStamp)
-  }
+  let options = [
+    {
+      title: 'Mobile Controller',
+      subtitle: 'Use your mobile phone as a remote controller.',
+      link: resolve('/mobile'),
+    },
+    {
+      title: 'Laptop Controller',
+      subtitle: 'Use your laptop as a remote controller.',
+      link: resolve('/pc'),
+    },
+  ]
 </script>
 
-<div class="w-full p-4">
-  <h1 class="mb-6 text-center text-2xl">Welcome to Soccer BT Remote</h1>
-  <button
-    class="w-full rounded-2xl bg-blue-500 p-6 text-2xl"
-    onclick={() => {
-      requestListenToOrientation(handleOrientation)
-    }}>CLICK ME TO START SENSOR CAPTURE</button
-  >
-
-  <div class="mt-6">
-    <div class="text-2xl">alpha: {alpha}</div>
-    <div class="text-2xl">beta: {beta}</div>
-    <div class="text-2xl">gamma: {gamma}</div>
-    <div class="text-2xl">timeStamp: {timeStamp}</div>
-  </div>
-</div>
+<OptionsPage {options} />
